a51336e5125058cb35e6e3fe63b39577a72c45a8
Jurrian Lammerts <0914922@hr.nl>
2017-09-27 14:58:54 +0200

    Login created

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Auth;
 use View;
 
 
@@ -29,6 +30,15 @@ class AuthController extends BaseController
             'name' => Input::get('username'),
             'password' => Input::get('password')
         ), false);
+
+        if(!$auth) {
+            return Redirect::route('login')->withErrors(array(
+                'Invalid credentials were provided.'
+            ));
+        }
+
+        return Redirect::route('home');
+
 
     }
 }
