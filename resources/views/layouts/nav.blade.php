bdb25e8cce61421888301874956140761c6457d8
Jurrian Lammerts <0914922@hr.nl>
2017-10-30 20:23:35 +0100

    Lists implemented

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -22,8 +22,8 @@
 
 <nav class="side-nav">
     <ul>
-    
-         <li><a href="{{ route('home') }}"><i class="fa fa-line-chart"></i> Dashboard</a></li>
+        <li><a href=><img src="http://www.bighedz.com/370-thickbox_default/donald-trump-life-size-celebrity-face-mask.jpg"></a></li>
+        <li><a href="{{ route('home') }}"><i class="fa fa-line-chart"></i> Dashboard</a></li>
         <li><i class="fa fa-cog"></i> Settings</li>
         <li><a href="{{ route('logout') }}"><i class="fa fa-sign-out" ></i>Log out</a></li>
     </ul>
